d5d415a7289fef5f2ac92625d363cfe75c584de5
Jiseop Han <jshan2017@gmail.com>
2019-01-11 21:35:13 +0900

    commit1

--- routes/chat.js
+++ routes/chat.js
@@ -2,55 +2,32 @@ var express = require('express');
 var router = express.Router();
 var dateFormat = require('dateformat');
 var Crawler = require("crawler");
+var util= require('util');
+var yahooFinance = require('yahoo-finance');
 
 /* GET home page. */
 router.post('/', function(req, res, next) {
-
+    var serverTime = new Date();
     console.log(req.body); 
-    var action = req.body.result.action;
-    console.log('requested action: ' + action);
+    var action = req.body.queryResult.action;
     if(action == 'stock_info'){
-        var market = req.body.result.parameters.Stocks;
-        var reqDate = dateFormat(req.body.result.parameters.date, "fullDate");
+        var market = req.body.queryResult.parameters.Stocks;
+        var reqDate = dateFormat(req.body.queryResult.parameters.date, "fullDate");
         console.log('market: '+ market);
         console.log('date: '+reqDate);
-        if(reqDate == dateFormat(new Date(), "fullDate")){//today
-            var c = new Crawler({
-                maxConnections : 10,
-                // This will be called for each crawled page
-                callback : function (error, res2, done) {
-                    if(error){
-                        console.log(error);
-                    }else{
-                        var $ = res2.$;
-                        // $ is Cheerio by default
-                        //a lean implementation of core jQuery designed specifically for the server
-                        if(market == "KOSPI"){
-                            var price = $("#KOSPI_now").text();
-                            var change = $("#KOSPI_change").text();
-                            console.log('change: '+change);
-                            res.json({ 'speech': '오늘의 '+market+' 지수는 어제보다 '+change+'한 '+price + '입니다', 'displayText': 'tomorrow' });
-                        }
-                        else if(market == "KOSDAQ"){
-                            var price = $("#KOSDAQ_now").text();
-                            var change = $("#KOSDAQ_change").text();
-                            console.log('change: '+change);
-                            res.json({ 'speech': '오늘의 '+market+' 지수는 어제보다 '+change+'한 '+price + '입니다', 'displayText': 'tomorrow' });
-                        }
-                        else{
-                            var price = $("#KPI200_now").text();
-                            var change = $("#KPI200_change").text();
-                            console.log('change: '+change);
-                            res.json({ 'speech': '오늘의 '+market+' 지수는 어제보다 '+change+'한 '+price + '입니다', 'displayText': 'tomorrow' });
-                        }
-                    }
-                    done();
-                }
-            });
-            c.queue('https://finance.naver.com/sise/');
-           // res.json({ 'speech': msg[0], 'displayText': 'tomorrow' });
+        if(reqDate == dateFormat(serverTime, "fullDate")){//today
+            console.log('here today');
+            yahooFinance.historical({
+                symbol: 'AAPL',
+                from: '2019-01-01',
+                to: '2019-01-02',
+                // period: 'd'  // 'd' (daily), 'w' (weekly), 'm' (monthly), 'v' (dividends only)
+              }, function (err, quotes) {
+                    console.log(quotes);
+              });
         }
         else{//tomorrow
+            console.log('tomorrow never knows');
             res.json({ 'speech': 'hell', 'displayText': 'tomorrow' });
         }
     }
