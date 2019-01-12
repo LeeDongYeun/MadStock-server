5937a98d34952ca630a3ecddba53046b3f5cbd2a
Jiseop Han <jshan2017@gmail.com>
2019-01-12 16:49:59 +0900

    getting company stock info with V2 api implemented. has dow/nasdaq bug

--- routes/chat.js
+++ routes/chat.js
@@ -2,33 +2,90 @@ var express = require('express');
 var router = express.Router();
 var dateFormat = require('dateformat');
 var Crawler = require("crawler");
-var util= require('util');
-var yahooFinance = require('yahoo-finance');
-
+const path = require('path');
+var client = require('cheerio-httpcli');
+const sqlite3=require('sqlite3').verbose();
+var du = require('date-utils');
+var newDate = new Date();
+var time = newDate.toFormat('YYYY년 MM월 DD일 HH24:MI 현재:')
 /* GET home page. */
 router.post('/', function(req, res, next) {
-    var serverTime = new Date();
     console.log(req.body); 
+    console.log(time);
     var action = req.body.queryResult.action;
     if(action == 'stock_info'){
-        var market = req.body.queryResult.parameters.Stocks;
-        var reqDate = dateFormat(req.body.queryResult.parameters.date, "fullDate");
+        var market = req.body.queryResult.parameters.probs.trim();
+        var reqDate = req.body.queryResult.parameters.Date;
         console.log('market: '+ market);
         console.log('date: '+reqDate);
-        if(reqDate == dateFormat(serverTime, "fullDate")){//today
+        if(reqDate == 'Today'){//today
             console.log('here today');
-            yahooFinance.historical({
-                symbol: 'AAPL',
-                from: '2019-01-01',
-                to: '2019-01-02',
-                // period: 'd'  // 'd' (daily), 'w' (weekly), 'm' (monthly), 'v' (dividends only)
-              }, function (err, quotes) {
-                    console.log(quotes);
-              });
+            const dbPath = path.resolve('res', 'stocksUtf.db');
+            var db = new sqlite3.Database(dbPath ,(err) => {
+                if(err){
+                    console.error(err.message);
+                }
+                else{
+                    console.log('Connected to db!');
+                }
+                
+            });
+            let sql = "SELECT code FROM utftable WHERE name  =?";
+            var code;
+            db.get(sql, [market], (err, row) => {
+                if (err) {
+                    return console.error(err.message);
+                }
+                else{
+                    console.log('its: '+ row.code);
+                    code=row.code;
+                    console.log('code is :'+code);
+                    
+
+                    var url = "http://finance.yahoo.com/q/hp";
+                    
+                    client.fetch(url, {
+                            "s": code
+                    }, function(err, $, res2) {
+                        if (err) {
+                            console.log(err);
+                            return;
+                        }
+                    
+                        var str = $('#quote-header-info > div > div > div > span:nth-child(1)').text();
+                        str = str.replace(/,/g, "");
+                        str = str.substring(str.indexOf('KRW'), str.length);
+                        var arr = str.match(/\d*\.\d*/);
+                    
+                        console.log(code);
+                        console.log("Price: " + arr[0]);
+                        var point = '주가는 ';
+                        var sunit = '원';
+                        if(market=='KOSPI' || market=='KOSDAQ' || market=='KOSPI200' || market=='DOW' || market=='NASDAQ' || market=='S&P500'){
+                            point = '지수는 ';
+                            sunit = '포인트' ;
+                        }
+                        res.json({  "fulfillmentText": time+'\n'+market+"의 "+point+arr[0]+sunit+"입니다.",
+                        "fulfillmentMessages": [
+                          {
+                            "text": {
+                              "text": [
+                                time+'\n'+market+"의 "+point+arr[0]+sunit+"입니다."
+                              ]
+                            }
+                          }
+                        ],});
+                    });
+                    
+                }
+                
+                
+            });
+            
         }
         else{//tomorrow
             console.log('tomorrow never knows');
-            res.json({ 'speech': 'hell', 'displayText': 'tomorrow' });
+            
         }
     }
     
